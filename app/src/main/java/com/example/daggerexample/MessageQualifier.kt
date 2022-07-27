ff09bc95a2dbea5b93d305c4a15707a98b36116e
Sheenal Patel <spatel@spatel2022.local>
2022-07-27 10:43:16 -0300

    Qualifiers

--- app/src/main/java/com/example/daggerexample/MessageQualifier.kt
+++ app/src/main/java/com/example/daggerexample/MessageQualifier.kt
@@ -1,3 +1,20 @@
 package com.example.daggerexample
 
+import java.lang.annotation.Documented
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Qualifier
+
+/*
+Qualifiers are used when we don't want any typo errors with Named("") annotations and want to be safe specify our own annotations
+ */
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
 annotation class MessageQualifier()
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+annotation class EmailQualifier()
--- app/src/main/java/com/example/daggerexample/NotificationServiceModule.kt
+++ app/src/main/java/com/example/daggerexample/NotificationServiceModule.kt
@@ -2,12 +2,20 @@ package com.example.daggerexample
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
 
+    @MessageQualifier
     @Provides
     fun getMessageService(): NotificationService {
         return MessageService()
+    }
+
+    @EmailQualifier
+    @Provides
+    fun getEmailService(emailService: EmailService): NotificationService{
+        return emailService
     }
 }
